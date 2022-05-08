6a5d6cc9b6a493b5ba6cda5a0c99aa317ddb9fa5
Soturnus <rivaldo.valhalla@gmail.com>
2022-05-07 21:59:33 -0300

    obtendo post por id

---
+++ src/main/java/com/soturno/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.soturno.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.soturno.workshopmongo.domain.Post;
+import com.soturno.workshopmongo.repository.PostRepository;
+import com.soturno.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
